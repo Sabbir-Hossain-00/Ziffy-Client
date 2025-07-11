27af54324262484a4b5e65bde759c56b1133943f
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-07-12 01:17:26 +0600

    edited banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,3 +1,4 @@
+import { MdSearch } from "react-icons/md";
 import { useAxiosSecure } from "../../Hooks/useAxiosSecure";
 
 export const Banner = ({
@@ -14,21 +15,26 @@ export const Banner = ({
     setPosts(data);
   };
   return (
-    <section className="py-10">
-      <div className="py-20 mt-10 flex flex-col justify-center items-center bg-white rounded-2xl">
+    <section className="py-10 container mx-auto px-3 md:px-6 lg:px-8 xl:px-14 ">
+      <div className="relative md:py-30 py-14 mt-10 flex flex-col justify-center items-center bg-white rounded-2xl px-3">
+        <div className="absolute inset-0 bg-[url('https://i.ibb.co/NgmFDTmv/image.png')] bg-cover bg-center z-0 rounded-2xl" />
+        <div className="absolute rounded-2xl inset-0 bg-black/60 z-10" />
+
         <form
           onSubmit={handleSearch}
-          className="w-full flex justify-center items-center gap-0"
+          className="w-full flex justify-center items-center gap-0  z-20 "
         >
           <input
             type="text"
-            className="bg-white border border-gray-300 p-2 w-1/3 rounded"
+            className="bg-white border border-gray-300 p-2 md:pl-4 pl-3 md:w-1/3 w-3/4 py-3 rounded-l-full"
+            placeholder="Search ziffy"
             value={searchResult}
             onChange={(e) => setSearchResult(e.target.value)}
           />
-          <button className="btn">Search</button>
+          <button className="btn border-l-none py-6 rounded-r-full px-7">Search</button>
         </form>
-        <div className="flex gap-4 mt-6">
+        <div className="z-20 flex items-center flex-wrap gap-4 mt-6">
+          <p className="font-medium text-lg text-white">Popular topics :</p>
           {tags?.map((tag) => {
             return (
               <p
